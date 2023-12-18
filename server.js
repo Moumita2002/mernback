d5f94487f2316833b234511666f98287a63b7812
Moumita2002 <102172188+Moumita2002@users.noreply.github.com>
2023-12-18 13:18:11 +0530

    first

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const MONGODB_URL = "mongodb://127.0.0.1:27017/yoga";
+const app = express();
+
+// MiddleWares
+app.use(cors());
+app.use(express.json());
+mongoose.connect(MONGODB_URL);
+
+const db = mongoose.connection;
+db.on("error", (err) => {
+    console.error("Mongodb connnection error", err);
+});
+db.once("open", () => {
+    console.log("Mongodb is connected");
+});
+
+
+// Model
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    gender: {
+        type: String,
+        required: [true, 'Gender is required'],
+    },
+    age: {
+        type: String,
+        required: [true, 'Age is required'],
+    },
+    fees: {
+        type: String,
+        required: [true, 'Role is required'],
+    },
+    slot: {
+        type: String,
+        required: [true, 'Role is required'],
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+// Routes
+app.post('/register', async (req, res) => {
+    try {
+        const newUser = new User({
+            name: req.body.name,
+            gender: req.body.gender,
+            age: req.body.age,
+            fees: req.body.fees,
+            slot: req.body.slot
+        });
+
+        const savedUser = await newUser.save()
+        return res.status(201).send({
+            message: 'User Registered in Yoga class',
+            success: true,
+            savedUser
+        })
+    } catch (error) {
+        console.log('Error during registeration', error);
+        res.status(500).send({
+            message: 'Error during registration',
+            success: false,
+            error
+        })
+    }
+})
+
+// PORT
+const PORT = 8080;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server Running at port ${PORT} successfully!!`);
+})
